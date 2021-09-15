08a02f127bf6395f3d8f992954af938f9e5cf966
Cyber MacGeddon <cybermaggedon@gmail.com>
2021-09-15 10:43:48 +0100

    Added Python packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gnucash-uk-corptax",
+    version="0.1",
+    author="Cybermaggedon",
+    author_email="mark@cyberapocalypse.co.uk",
+    description="UK Companies House accounts filing for GnuCash users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cybermaggedon/gnucash-ch-filing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    download_url = "https://github.com/cybermaggedon/gnucash-uk-corptax/archive/refs/tags/v0.1.tar.gz",
+    install_requires=[
+        'lxml',
+        'requests'
+    ],
+    scripts=[
+        "scripts/ch-filing",
+    ]
+)
